807d734dfcbcecdeda358f9ae8ed25649dbae5e9
mix <mix.touch777@gmail.com>
2016-06-03 22:48:41 +0900

    擬似コード書いた

---
+++ game.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(){
+	int want_play_num; //最低保証プレイ回数（答えが解ってしまわないようにする）
+	int start;	//質問範囲の初め
+	int end;	//質問範囲の終わり
+	int answer;	//答えの数字
+	int start_limit;	//前半を絞り込む時の範囲の最大値
+	int end_limit;		//後半を絞り込む時の範囲の最小値
+	int play_num;		//回答回数。プレイ回数
+
+	/*
+	do{
+		//答えを入力させる。
+		//入力値のチェック
+		if(入力値が正しくない){
+			//正しく数値を入力してください
+			int input_ok = false;
+		}else{
+			int input_ok = true;
+		}
+	}while(input_ok);
+
+	//ゲームスタート
+	while(true){
+		//質問する。start-1 < x < end + 1 x = ?
+		//入力させる
+		//入力値のチェック
+		if(入力値が正しくない){
+			//正しく数値を入力してください
+			continue;
+		}
+		//正解チェック
+		if(入力値 == answer){
+			//正解です。おめでとうございます。play_num回で答えられました。プログラムの終了
+			break;
+		}
+		//不正解。範囲を絞る
+		if(奇数階数){
+			//奇数回数目なら前半を絞る
+			//start+1からstart_limitの中からランダムで一つ選んでstartに代入する。
+			start_limit = start_limi + 1;
+
+		}else{
+			//偶数階数目なら後半を絞る
+			//end-1からend_limitの中からランダムで一つ選んでendに代入する。
+			end_limit = end_limit - 1;
+		}
+	}
+
+	*/
+	return 0;
+}
+
