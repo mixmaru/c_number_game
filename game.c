b963959c6f435a72f680dd1421c58f8533d84095
mix <mix.touch777@gmail.com>
2016-06-04 23:01:13 +0900

    初期化処理と、ゲーム本体実行処理を関数に分けた。

--- game.c
+++ game.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
 enum SqueezeSide { LEFT, RIGHT };//数字を絞る方向を記録
-int main(){
-	int start = 1;	//質問範囲の初め
-	int end = 99;	//質問範囲の終わり
-	int answer;	//答えの数字
-	int start_limit;	//前半を絞り込む時の範囲の最大値
-	int end_limit;		//後半を絞り込む時の範囲の最小値
-	int answer_num = 0; 	//回答回数
 
+void init(int *answer, int *start_limit, int *end_limit, int start, int end){
 	/*
 	do{
 		//答えを入力させる。
@@ -26,8 +20,13 @@ int main(){
 	answer = 入力された数字
 	start_limit = answer - 1;
 	end_limit = answer + 1;
+	*/
+}
 
+//結果(クリア or ゲームオーバー)とクリアの場合は回答回数を返す
+void executeGame(int answer, int start_limit, int end_limit, int start, int end, int *result, int *answer_num){
 	//ゲームスタート
+	/*
 	while(true){
 		//質問する。start-1 < x < end + 1 x = ?
 		//入力させる
@@ -38,14 +37,18 @@ int main(){
 		}
 		//回答回数を追加
 		answer_num = answer_num + 1;
+
 		//正解チェック
 		if(入力値 == answer){
-			//正解です。おめでとうございます。answer_num回で答えられました。プログラムの終了
-			break;
+			//正解
+			*result = 1;
+			return;
 		}
+
 		//不正解。範囲を絞る。
 		//2回いないに範囲を絞れない状況はゲームオーバー。LEFT側、RIGHT側どちらも範囲を絞れなかった = 最小まで絞っても答えられなかった。
-		for(int i=0; i<2; i++){
+		int try_count=0;
+		for(; trytry_count<2; trtrytry_count++){
 			if(SqueezeSide == LEFT){
 				//前半を絞る
 				//startからstart_limitの中からランダムで一つ選んでstartに代入する。
@@ -69,10 +72,36 @@ int main(){
 				}
 			}
 		}
-		//ゲームオーバー
-		return 0;
+		if(trytry_count >= 2){
+			//ゲームオーバー
+			*result = 0;
+			return;
+		}
 	}
+	*/
+}
 
+int main(){
+	int start = 1;	//質問範囲の初め
+	int end = 99;	//質問範囲の終わり
+	int answer;	//答えの数字
+	int start_limit;	//前半を絞り込む時の範囲の最大値
+	int end_limit;		//後半を絞り込む時の範囲の最小値
+	int answer_num = 0; 	//回答回数
+
+
+	/*
+	//初期化処理
+	init(&answer, &start_limit, &end_limit, start, end);
+
+	int result = 0;//ゲーム結果 0:ゲームオーバー、1:ゲームクリア
+	executeGame(answer, start_limit, end_limit, start, end, &result, &answer_num);
+
+	if(result){
+		//ゲームクリア。answre_count回でクリアしました
+	}else{
+		//ゲームオーバー。
+	}
 	*/
 	return 0;
 }
